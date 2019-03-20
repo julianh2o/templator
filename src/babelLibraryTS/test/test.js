fca7e5f73635422b92588943278ca599fb5b92e6
Julian Hartline <julianh2o@gmail.com>
2019-03-20 12:21:46 -0700

    Safety commit, about to tweak babel library TS

---
+++ src/babelLibraryTS/test/test.js
@@ -0,0 +1,11 @@
+import "chai/register-assert";
+import * as winstonUtils from "@atypon/winston-utils";
+import winston from "winston";
+
+describe("Test new library", async function() {
+    describe("Tests", async function() {
+        it("should sanity check tests",async function() {
+            assert.isOk(true);
+        });
+    });
+});
